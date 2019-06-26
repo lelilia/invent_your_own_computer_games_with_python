1d8c6fb960a03fae6a797cb945f20b9fed45ad46
Leli <33942105+lelilia@users.noreply.github.com>
2019-06-26 14:32:54 +0200

    Create hallo.py

---
+++ hallo.py
@@ -0,0 +1,6 @@
+# https://inventwithpython.com/invent4thed/
+# Dieses Programm fragt nach deinem Namen und begrüßt dich
+print('Hallo!')
+print('Wie heißt du?')
+name  = input()
+print('Nett dich kennen zu lernen, '+name+'!')
